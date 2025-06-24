57155c5372bacad521f463b9a0a9612796a6c60a
William Hsieh <wh31110@gmail.com>
2025-06-24 23:45:34 +0800

    refactor: return popup value when creating popups

--- lua/overlook/ui.lua
+++ lua/overlook/ui.lua
@@ -2,7 +2,7 @@ local M = {}
 
 ---Creates and opens a floating window viewing the target buffer.
 ---@param opts OverlookPopupOptions
----@return { win_id: integer, buf_id: integer } | nil
+---@return OverlookPopup?
 function M.create_popup(opts)
   local popup = require("overlook.popup").new(opts)
   if not popup then
@@ -12,7 +12,7 @@ function M.create_popup(opts)
   local stack = require("overlook.stack").win_get_stack(popup.orginal_win_id)
   stack:push(popup)
 
-  return { win_id = popup.win_id, buf_id = popup.opts.target_bufnr }
+  return popup
 end
 
 return M
